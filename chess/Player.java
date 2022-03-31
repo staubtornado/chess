568440a63b84f992f472fee07f86ff6fce0cceec
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-03-31 20:25:17 +0200

    Fix repo bug

---
+++ chess/Player.java
@@ -0,0 +1,16 @@
+package chess;
+
+public class Player {
+    private final String name;
+    private final int id;
+
+    public Player(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {return name;}
+    public int getId() {
+        return id;
+    }
+}
